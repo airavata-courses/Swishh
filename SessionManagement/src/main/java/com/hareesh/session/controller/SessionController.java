b3b58e2c508e31d5aa693c3c5b332e41e0c97ad7
hbondala <66460697+hbondala@users.noreply.github.com>
2021-02-28 20:42:36 -0500

    Update SessionController.java

--- SessionManagement/src/main/java/com/hareesh/session/controller/SessionController.java
+++ SessionManagement/src/main/java/com/hareesh/session/controller/SessionController.java
@@ -8,10 +8,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.google.gson.Gson;
 import com.hareesh.session.service.SessionService;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class SessionController {
 	
