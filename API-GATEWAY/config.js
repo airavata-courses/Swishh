eb50cd22c48ca1a9e345e32bb5bace4c58bef533
Hareesh Bondalapati <hai.hareeshb@gmail.com>
2021-04-22 03:28:21 -0400

    modfied api gateway

--- API-GATEWAY/config.js
+++ API-GATEWAY/config.js
@@ -5,7 +5,7 @@ const config = {
     },
 
     routeURLS: {
-      userService : "http://127.0.0.1:5003",
+      userService : "http://user-service:5003",
       imageStorage : "http://127.0.0.1:8081",
       sessionManagement : "http://127.0.0.1:8082"
     }
